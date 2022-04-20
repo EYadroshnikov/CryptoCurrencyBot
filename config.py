59a6aafffd18fe570e44c8c48f9561af9124f4cd
EYadroshnikov <yadegor1375@gmail.com>
2022-04-20 20:38:41 +0600

    added second key

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
+from os import waitid
+
+
 token = "5319244339:AAFx8P1g6cLtHThGKa5EXazbqUO3bi55pC4"
 x_coin_api_key = "B3D2B3FA-3CE5-48BD-B151-B535743C3789"
+# x_coin_api_key = "EF0ACB2E-A755-4E4F-9AD3-90684E71551D"
